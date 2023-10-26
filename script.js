388d5fbb8ca2eace38e8f779610fae4f076d348a
donuts-and <89621960+donuts-and@users.noreply.github.com>
2023-10-26 07:36:44 -0500

    Switch from snake_case to camelCase
    Also tweak some key labels and variable names

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-let layer_base = [
+let layerBase = [
   ["Q" , "W" , "F" , "P"  , "B"    , null , null, "J"    , "L"  , "U" , "Y" , "'"],
   ["A" , "R" , "S" , "T"  , "G"    , null , null, "M"    , "N"  , "E" , "I" , "O"],
   ["Z" , "X" , "C" , "D"  , "V"    , null , null, "K"    , "H"  , "," , "." , "/"],
   [null, null, null, "NAV", "shift", "tab", ";" , "space", "SYM", null, null, null]
 ]
 
-let layer_nav = [
+let layerNav = [
   ["esc"  , "enter", " "      , " "     , " ", null, null, "pgup", "home"  , "up"  , "end"  , "del"],
   ["alt"  , "gui"  , "shift"  , "ctrl"  , " ", null, null, "pgdn", "left"  , "down", "right", "bksp"],
   ["alt:S", "gui:S", "shift:S", "ctrl:S", " ", null, null, " "   , "S-tab", "tab" , " "    , " "],
   [null   , null   , null     , "NAV"   , " ", " " , " " , " "   , " "     , null  , null   , null]
 ]
 
-let layer_sym = [
+let layerSym = [
   ["!" , "@" , "#" , "$", "%", null, null, "^" , "&"  , "*" , "-" , "="],
   ["1" , "2" , "3" , "4", "5", null, null, "6" , "7"  , "8" , "9" , "0"],
   ["[" , "]" , "{" , "}", "`", null, null, "\\", "("  , ")" , "." , "/"],
   [null, null, null, " ", " ", " " , " " , " " , "SYM", null, null, null]
 ]
 
-let layer_fun = [
-  ["rgb\nspi", "rgb\nhui", "rgb\nsai", "rgb\nvai", "rgb\nmod" , null, null, " "   , "F1" , "F2" , "F3" , "F4"],
-  ["rgb\nspd", "rgb\nhud", "rgb\nsad", "rgb\nvad", "rgb\nrmod", null, null, "caps", "F5" , "F6" , "F7" , "F8"],
+let layerFun = [
+  ["rgb\nsp+", "rgb\nhu+", "rgb\nsa+", "rgb\nva+", "rgb\nmod" , null, null, " "   , "F1" , "F2" , "F3" , "F4"],
+  ["rgb\nsp-", "rgb\nhu-", "rgb\nsa-", "rgb\nva-", "rgb\nrmod", null, null, "caps", "F5" , "F6" , "F7" , "F8"],
   ["alt:S"   , "gui:S"   , "shift:S" , "ctrl:S"  , "rgb\ntog" , null, null, "ins" , "F9" , "F10", "F11", "F12"],
   [null      , null      , null      , "EXT"     , " "        , " " , " " , " "   , "SYM", null , null , null]
 ]
 
-let combo_layer = "base";
+let comboLayer = "base";
 let combos = [
   {
     "input": ["D", "H"],
@@ -70,71 +70,70 @@ let combos = [
   },
 ]
 
-let col_class_left = ["col-pinky", "col-ring", "col-middle", "col-index", "col-inner", "col-thumb"]
-let col_class_right = ["col-thumb", "col-inner", "col-index", "col-middle", "col-ring", "col-pinky"]
-let col_class = col_class_left.concat(col_class_right)
-let col_hand = ["left", "left", "left", "left", "left", "left", "right", "right", "right", "right", "right", "right"]
-let row_class = ["", "", "", "mod"]
+let colClassLeft = ["col-pinky", "col-ring", "col-middle", "col-index", "col-inner", "col-thumb"]
+let colClassRight = ["col-thumb", "col-inner", "col-index", "col-middle", "col-ring", "col-pinky"]
+let colClass = colClassLeft.concat(colClassRight)
+let colHand = ["left", "left", "left", "left", "left", "left", "right", "right", "right", "right", "right", "right"]
+let rowClass = ["", "", "", "mod"]
 
-let tippy_placement = ["top", "left", "right", "bottom"]
+let tippyPlacements = ["top", "left", "right", "bottom"]
 
-function get_combos(layer, key_label) {
-  if (layer != combo_layer) {
+function getCombos(layer, keyLabel) {
+  if (layer !== comboLayer) {
     return []
   }
   let c = [];
   for (let combo of combos) {
-    if (combo.input.includes(key_label)) {
+    if (combo.input.includes(keyLabel)) {
       c.push(combo)
     }
   }
   return c;
 }
 
-function render_layout(layer, keys) {
+function renderLayout(layer, keys) {
   for (let col = 0; col < keys[0].length; col++) {
-    let base_container = document.getElementById("layer-" + layer + "-" + col_hand[col])
-    let column = document.createElement("div")
-    column.className = "col " + col_class[col]
-    base_container.append(column)
+    let handElement = document.getElementById("layer-" + layer + "-" + colHand[col])
+    let colElement = document.createElement("div")
+    colElement.className = "col " + colClass[col]
+    handElement.append(colElement)
     for (let row = 0; row < keys.length; row++) {
-      let key_id = `r${row}c${col}`
-      if (!keys[row][col]) {
+      let key = keys[row][col];
+      if (!key) {
         continue
       }
-      let tokens = keys[row][col].split(":")
-      let key_label = tokens[0]
-      let key_mod = tokens[1] || ""
-      let key = document.createElement("span")
-      key.id = key_id
-      key.classList.add("key")
-      if (row_class[row]) {
-        key.classList.add(row_class[row])
+      let tokens = key.split(":")
+      let keyLabel = tokens[0]
+      let keyMod = tokens[1] || ""
+      let keyElement = document.createElement("span")
+      keyElement.classList.add("key")
+      if (rowClass[row]) {
+        keyElement.classList.add(rowClass[row])
       }
-      let key_combos = get_combos(layer, key_label);
+      let key_combos = getCombos(layer, keyLabel);
       if (key_combos.length > 0) {
-        key.classList.add("combo")
+        keyElement.classList.add("combo")
         for (let i = 0; i < key_combos.length; i++){
           let combo = key_combos[i];
-          tippy(key, {
+          tippy(keyElement, {
             content: `${combo.input.join("")} combo: ${combo.output}`,
-            placement: tippy_placement[i]
+            placement: tippyPlacements[i]
           })
         }
       }
-      if (key_mod) {
-        key.classList.add(key_mod)
+      if (keyMod) {
+        keyElement.classList.add(keyMod)
       }
-      key.innerText = key_label
-      column.append(key)
+      keyElement.innerText = keyLabel
+      colElement.append(keyElement)
     }
   }
 }
 
-render_layout("base", layer_base);
-render_layout("nav", layer_nav);
-render_layout("sym", layer_sym);
-render_layout("fun", layer_fun);
+renderLayout("base", layerBase);
+renderLayout("nav", layerNav);
+renderLayout("sym", layerSym);
+renderLayout("fun", layerFun);
 
 tippy(".S", {
   content: "sticky",
