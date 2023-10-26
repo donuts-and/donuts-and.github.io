fab9314c694c438ca4a0b52cbe5310d112a7b39f
donuts-and <89621960+donuts-and@users.noreply.github.com>
2023-10-26 08:24:09 -0500

    Move all key classes to single-letter layer annotations
    - Switch font to JetBrains Mono
    - Add homing key indicator

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 let layerBase = [
-  ["Q" , "W" , "F" , "P"  , "B"    , null , null, "J"    , "L"  , "U" , "Y" , "'"],
-  ["A" , "R" , "S" , "T"  , "G"    , null , null, "M"    , "N"  , "E" , "I" , "O"],
-  ["Z" , "X" , "C" , "D"  , "V"    , null , null, "K"    , "H"  , "," , "." , "/"],
-  [null, null, null, "NAV", "shift", "tab", ";" , "space", "SYM", null, null, null]
+  ["Q" , "W" , "F" , "P"    , "B"      , null   , null , "J"      , "L"    , "U" , "Y" , "'"],
+  ["A" , "R" , "S" , "T:H"  , "G"      , null   , null , "M"      , "N:H"  , "E" , "I" , "O"],
+  ["Z" , "X" , "C" , "D"    , "V"      , null   , null , "K"      , "H"    , "," , "." , "/"],
+  [null, null, null, "NAV:M", "shift:M", "tab:M", ";:M", "space:M", "SYM:M", null, null, null]
 ]
 
 let layerNav = [
-  ["esc"  , "enter", " "      , " "     , " ", null, null, "pgup", "home"  , "up"  , "end"  , "del"],
-  ["alt"  , "gui"  , "shift"  , "ctrl"  , " ", null, null, "pgdn", "left"  , "down", "right", "bksp"],
-  ["alt:S", "gui:S", "shift:S", "ctrl:S", " ", null, null, " "   , "S-tab", "tab" , " "    , " "],
-  [null   , null   , null     , "NAV"   , " ", " " , " " , " "   , " "     , null  , null   , null]
+  ["esc"  , "enter", " "      , " "     , " " , null, null, "pgup", "home"  , "up"  , "end"  , "del"],
+  ["alt"  , "gui"  , "shift"  , "ctrl:H", " " , null, null, "pgdn", "left:H", "down", "right", "bksp"],
+  ["alt:S", "gui:S", "shift:S", "ctrl:S", " " , null, null, " "   , "S-tab" , "tab" , " "    , " "],
+  [null   , null   , null     , "NAV:M" , ":M", ":M", ":M", ":M"  , ":M"    , null  , null   , null]
 ]
 
 let layerSym = [
-  ["!" , "@" , "#" , "$", "%", null, null, "^" , "&"  , "*" , "-" , "="],
-  ["1" , "2" , "3" , "4", "5", null, null, "6" , "7"  , "8" , "9" , "0"],
-  ["[" , "]" , "{" , "}", "`", null, null, "\\", "("  , ")" , "." , "/"],
-  [null, null, null, " ", " ", " " , " " , " " , "SYM", null, null, null]
+  ["!" , "@" , "#" , "$"  , "%" , null, null, "^" , "&"    , "*" , "-" , "="],
+  ["1" , "2" , "3" , "4:H", "5" , null, null, "6" , "7:H"  , "8" , "9" , "0"],
+  ["[" , "]" , "{" , "}"  , "`" , null, null, "\\", "("    , ")" , "." , "/"],
+  [null, null, null, ":M" , ":M", ":M", ":M", ":M", "SYM:M", null, null, null]
 ]
 
 let layerFun = [
-  ["rgb\nsp+", "rgb\nhu+", "rgb\nsa+", "rgb\nva+", "rgb\nmod" , null, null, " "   , "F1" , "F2" , "F3" , "F4"],
-  ["rgb\nsp-", "rgb\nhu-", "rgb\nsa-", "rgb\nva-", "rgb\nrmod", null, null, "caps", "F5" , "F6" , "F7" , "F8"],
-  ["alt:S"   , "gui:S"   , "shift:S" , "ctrl:S"  , "rgb\ntog" , null, null, "ins" , "F9" , "F10", "F11", "F12"],
-  [null      , null      , null      , "EXT"     , " "        , " " , " " , " "   , "SYM", null , null , null]
+  ["rgb\nsp+", "rgb\nhu+", "rgb\nsa+", "rgb\nva+"  , "rgb\nmod" , null, null, " "   , "F1"   , "F2" , "F3" , "F4"],
+  ["rgb\nsp-", "rgb\nhu-", "rgb\nsa-", "rgb\nva-:H", "rgb\nrmod", null, null, "caps", "F5:H" , "F6" , "F7" , "F8"],
+  ["alt:S"   , "gui:S"   , "shift:S" , "ctrl:S"    , "rgb\ntog" , null, null, "ins" , "F9"   , "F10", "F11", "F12"],
+  [null      , null      , null      , "EXT:M"     , ":M"       , ":M", ":M", ":M"  , "SYM:M", null , null , null]
 ]
 
 let comboLayer = "base";
@@ -74,7 +74,6 @@ let colClassLeft = ["col-pinky", "col-ring", "col-middle", "col-index", "col-inn
 let colClassRight = ["col-thumb", "col-inner", "col-index", "col-middle", "col-ring", "col-pinky"]
 let colClass = colClassLeft.concat(colClassRight)
 let colHand = ["left", "left", "left", "left", "left", "left", "right", "right", "right", "right", "right", "right"]
-let rowClass = ["", "", "", "mod"]
 
 let tippyPlacements = ["top", "left", "right", "bottom"]
 
@@ -107,9 +106,6 @@ function renderLayout(layer, keys) {
       let keyMod = tokens[1] || ""
       let keyElement = document.createElement("span")
       keyElement.classList.add("key")
-      if (rowClass[row]) {
-        keyElement.classList.add(rowClass[row])
-      }
       let key_combos = getCombos(layer, keyLabel);
       if (key_combos.length > 0) {
         keyElement.classList.add("combo")
